0bf817b3fb5e780f368eeae468f467aca0ce4362
Przemysław Grądzki <przemek.gradzki@gmail.com>
2023-10-03 20:06:32 +0200

    Use the environment variable name TMPDIR instead of TMP
    Signed-off-by: Przemysław Grądzki <przemek.gradzki@gmail.com>

--- rdf-toolkit.sh
+++ rdf-toolkit.sh
@@ -13,19 +13,19 @@ curl --version &>/dev/null || { echo "ERR: missing curl" ; read -n 1 -s -r -p "P
 
 java --version &>/dev/null || { echo "ERR: missing java - download e.g. from https://openjdk.org/install/ and install" ; read -n 1 -s -r -p "Press any key to continue...." ; exit 1 ; }
 
-test -n "${JARP}" || export JARP="${TMP:-/tmp}"
+test -n "${JARP}" || export JARP="${TMPDIR:-/tmp}"
 test -e "${JARP}"/rdf-toolkit.jar || { echo "INFO: downloading RDF Toolkit to \"${JARP}/rdf-toolkit.jar\"..." ; curl  --output-dir "${JARP}" -L -O "${JARURL}"/rdf-toolkit.jar 2>/dev/null ; echo "" ; }
 test -e "${JARP}"/rdf-toolkit.jar || { echo "ERR: rdf-toolkit.jar cannot be downloaded" ; read -n 1 -s -r -p "Press any key to continue...." ; exit 1 ; }
 
-export P="java -jar ${JARP}/rdf-toolkit.jar --source \"${1}\" --target \"${TMP}/rdf-toolkit.ttl\" --use-dtd-subset --inline-blank-nodes --infer-base-iri"
+export P="java -jar ${JARP}/rdf-toolkit.jar --source \"${1}\" --target \"${TMPDIR}/rdf-toolkit.ttl\" --use-dtd-subset --inline-blank-nodes --infer-base-iri"
 
 echo "INFO: ${P}"
 ${P}
 
 echo ""
 
-diff -u "${1}" "${TMP}/rdf-toolkit.ttl" &>/dev/null || { echo "WARN: move \"${TMP}/rdf-toolkit.ttl\" \"${1}\"" ; mv -i -v "${TMP}/rdf-toolkit.ttl" "${1}" ; }
-rm -f "${TMP}/rdf-toolkit.ttl"
+diff -u "${1}" "${TMPDIR}/rdf-toolkit.ttl" &>/dev/null || { echo "WARN: move \"${TMPDIR}/rdf-toolkit.ttl\" \"${1}\"" ; mv -i -v "${TMPDIR}/rdf-toolkit.ttl" "${1}" ; }
+rm -f "${TMPDIR}/rdf-toolkit.ttl"
 
 read -n 1 -s -r -p "Press any key to continue...."
 echo ""
